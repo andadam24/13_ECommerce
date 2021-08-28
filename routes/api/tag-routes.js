7a6f8ad79f918f8aee705b64ada0083ea6634b59
Adam And <andadam24@gmail.com>
2021-08-28 16:20:15 -0400

    updated tag-routes get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Category.findAll({
+      include: [{model: Product, through: ProductTag, as: 'tagproduct'}]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product, through: ProductTag, as: 'tagproduct'}]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag associated with that ID found !' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
