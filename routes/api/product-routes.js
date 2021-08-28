b837dbd709fb353f31bb910d79ea8947737108e4
Adam And <andadam24@gmail.com>
2021-08-28 16:07:57 -0400

    updated spelling error in through*

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{model: Category}, {model: Tag, thorugh: ProductTag, as: 'producttags'}]
+      include: [{model: Category}, {model: Tag, through: ProductTag, as: 'producttags'}]
     });
     if (!productData) {
       res.status(404).json({ message: 'No productdata found !' });
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{model: Category}, {model: Tag, thorugh: ProductTag, as: 'producttags'}]
+      include: [{model: Category}, {model: Tag, through: ProductTag, as: 'producttags'}]
     });
     if (!productData) {
       res.status(404).json({ message: 'No product associated with that ID found !' });
