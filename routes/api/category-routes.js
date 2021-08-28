1f32be9535e1c45124b84cc5b1d848da7ef0c5c4
Adam And <andadam24@gmail.com>
2021-08-28 15:34:18 -0400

    category routes get routes updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,41 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   // find all categories
   // be sure to include its associated Products
-});
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product, as: 'products'}]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   // find one category by its `id` value
   // be sure to include its associated Products
-});
 
 router.post('/', (req, res) => {
   // create a new category
