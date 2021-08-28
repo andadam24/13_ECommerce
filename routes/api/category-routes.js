4ca246b6328dc1d43025e8d6e3c4a0bb69d54fa4
Adam And <andadam24@gmail.com>
2021-08-28 15:44:01 -0400

    updated category routes post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,11 +39,18 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  
   // update a category by its `id` value
 });
 
