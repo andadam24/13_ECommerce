e00d3d30834a555fca2086c98879f0e044e38146
Adam And <andadam24@gmail.com>
2021-08-28 15:48:54 -0400

    updated category-routes delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,11 +50,27 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  
+
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete a category by its `id` value
 });
 
